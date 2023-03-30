cf71ea7541d547998792b0057c39fd90829ac963
Patrick Lauher <plauher14@gmail.com>
2023-03-30 13:16:37 -0500

    add class SpeedConverter

---
+++ src/SpeedConverter.java
@@ -0,0 +1,26 @@
+public class SpeedConverter {
+
+    public static void main(String[] args) {
+
+        printConversion(6);
+        printConversion(-5);
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            return -1;
+        }
+
+        return Math.round(kilometersPerHour / 1.609);       // returns a long type
+    }
+
+    public static void printConversion(double kilometersPerHour) {
+
+        if (kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        } else {
+            System.out.println(kilometersPerHour + " km/h = " + toMilesPerHour(kilometersPerHour) + " mi/h");
+        }
+    }
+}
