7fa9eafa150cbe5a150ce067dd15f2be6f3de6a9
Patrick Lauher <plauher14@gmail.com>
2023-04-04 15:08:21 -0500

    add method main to DecimalComparator

--- src/DecimalComparator.java
+++ src/DecimalComparator.java
@@ -1,12 +1,16 @@
 public class DecimalComparator {
 
-    //  the purpose of the method is to use casting
+    public static boolean main(String[] args) {
 
-    public static boolean areEqualByThreeDecimalPlaces(double num1, double num2) {
+        return areEqualByThreeDecimalPlaces(4.123, 4.123);
 
-        int iNum1 = (int) (num1 * 1000);
-        int iNum2 = (int) (num2 * 1000);
-
-        return iNum1 - iNum2 == 0;
     }
+        //  the purpose of the method is to use casting
+        public static boolean areEqualByThreeDecimalPlaces ( double num1, double num2){
+
+            int iNum1 = (int) (num1 * 1000);
+            int iNum2 = (int) (num2 * 1000);
+
+            return iNum1 - iNum2 == 0;
+        }
 }
